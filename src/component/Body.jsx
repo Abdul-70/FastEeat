ef1ff329ae89a97e586797312fed717f57e1cd24
AbdurRahim999 <141570882+AbdurRahim999@users.noreply.github.com>
2026-01-29 10:24:18 +0530

    using swiggy fake api

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -14,7 +14,9 @@ const Body = () => {
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetch(
-        "https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=26.868394&lng=80.931418&carousel=true&third_party_vendor=1"
+      //   "https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=26.868394&lng=80.931418&carousel=true&third_party_vendor=1"
+      // const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139&lng=77.2090&page_type=DESKTOP_WEB_LISTING"
       );
       const json = await data.json();
       console.log(json);
