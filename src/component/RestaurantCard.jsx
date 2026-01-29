d2ac6cbb94f03a1399b2a6902512fe2218954715
AbdurRahim999 <141570882+AbdurRahim999@users.noreply.github.com>
2026-01-29 14:29:57 +0530

    update restaurant UI

--- src/component/RestaurantCard.jsx
+++ src/component/RestaurantCard.jsx
@@ -14,10 +14,10 @@ const RestaurantCard = ({data}) => {
   } = data.info
 
   return (
-    <div className="shadow cursor-pointer hover:shadow-blue-400 rounded  w-55 m-5  ">
+    <div className="shadow cursor-pointer hover:shadow-blue-400 rounded  w-50 m-2  ">
       <div className="flex justify-center">
         <img
-          className="w-50 mt-2 rounded"
+          className="w-45 mt-2 rounded"
           src={CDN_URL+ cloudinaryImageId}
           alt=""
         />
